8e24dea285b633d5344cbe118c2b601c8f5a8c1b
MatiasAGomezJ <mgomez@cifpfbmoll.eu>
2022-01-31 15:17:34 +0100

    First commit

--- src/main/java/edu/poniperro/gildedrose/App.java
+++ src/main/java/edu/poniperro/gildedrose/App.java
@@ -5,6 +5,6 @@ import edu.poniperro.gildedrose.domain.*;
 public class App {
     public static void main(String[] args){
         GildedRose shop = new GildedRose();
-        System.out.println();
+        System.out.println(shop.toString());
     }
 }
